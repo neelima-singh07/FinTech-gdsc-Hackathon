1dd5e0d49c6e795c4f9935cf4c3911be1d00e1ed
neelima-singh07 <neelima.arun2007@gmail.com>
2025-04-10 12:24:53 +0530

    Initial commit: Added expense tracker dashboard

---
+++ src/expanse_tracker.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+
+const ApexChart = () => {
+  const [state] = React.useState({
+    series: [44, 55, 41, 17, 15],
+    options: {
+      chart: {
+        type: 'donut',
+      },
+      labels: ['Food', 'Transport', 'Entertainment', 'Bills', 'Others'],
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+    },
+  });
+
+  return (
+    <div id="chart">
+      <ReactApexChart 
+        options={state.options} 
+        series={state.series} 
+        type="donut" 
+        height={350}
+      />
+    </div>
+  );
+};
+
+export default ApexChart; // Must be default export
